bd3ea24379e0d94dcad9a917ad8fc1c377a7c53e
Gayathri <gayathri6@gatech.edu>
2013-03-31 22:24:39 -0400

    Download video from URL , Get file size from the downloaded file

---
+++ youtube_get_video.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $url = $ARGV[0] or die "\nError: You need to specify a YouTube URL\n\n";
+
+ 
+my $prefix = defined($ARGV[1]) ? $ARGV[1] : "";
+
+
+my $html = `wget -Ncq -e "convert-links=off" --keep-session-cookies --save-cookies /dev/null --no-check-certificate "$url" -O-`  or die  "\nThere was a problem downloading the HTML file.\n\n";
+
+
+my ($title) = $html =~ m/<title>(.+)<\/title>/si;
+$title =~ s/[^\w\d]+/_/g;
+$title =~ s/_youtube//ig;
+$title =~ s/^_//ig;
+$title = lc ($title);
+
+
+my ($download) = $html =~ /"url_encoded_fmt_stream_map"([\s\S]+?)\,/ig;
+
+$download =~ s/\:\ \"//;
+$download =~ s/%3A/:/g;
+$download =~ s/%2F/\//g;
+$download =~ s/%3F/\?/g;
+$download =~ s/%3D/\=/g;
+$download =~ s/%252C/%2C/g;
+$download =~ s/%26/\&/g;
+$download =~ s/sig=/signature=/g;
+$download =~ s/\\u0026/\&/g;
+$download =~ s/(type=[^&]+)//g;
+$download =~ s/(fallback_host=[^&]+)//g;
+$download =~ s/(quality=[^&]+)//g;
+
+
+my ($signature) = $download =~ /(signature=[^&]+)/;
+my ($youtubeurl) = $download =~ /(http.+)/;
+$youtubeurl =~ s/&signature.+$//;
+
+
+$download = "$youtubeurl\&$signature";
+
+
+$download =~ s/&+/&/g;
+$download =~ s/&itag=\d+&signature=/&signature=/g;
+
+
+
+print "\n Download: $prefix$title.webm\n\n";
+
+
+system("wget -Ncq -e \"convert-links=off\" --load-cookies /dev/null --tries=50 --timeout=45 --no-check-certificate \"$download\" -O $prefix$title.webm &");
+
+
+my ($filesize) = -s "$prefix$title.webm";
+print $filesize;
+
+#### EOF #####
